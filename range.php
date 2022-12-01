d73e862e0af24c1315b765a2cb86b3d906ef6020
Gabriel Gomes Tatsch <gomestatschgabriel@gmail.com>
2022-12-01 14:59:08 -0300

    Alterated.

---
+++ range.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+<head>
+  <meta charset='UTF-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='shortcut icon' href='#'>
+  <script src='' defer></script>
+  <title></title>
+</head>
+<body>
+  <?php
+    $x = range("a", "x");
+    print_r($x);
+
+    $x = range(11, 1, 2);
+    print_r($x);
+  ?>
+</body>
+</html>
