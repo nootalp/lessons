8a2db91d86fb7decd89af79ebfa82d73d56f5144
Gabriel Gomes Tatsch <gomestatschgabriel@gmail.com>
2022-12-01 17:45:51 -0300

    new_release

---
+++ key_value.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+<head>
+  <meta charset='UTF-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='shortcut icon' href='#'>
+  <script src='' defer></script>
+  <title></title>
+</head>
+<body>
+  <?php
+    $list = ["Zé" => 10, "Zéquinha" => 4];
+
+    echo $list["Zé"] . "\n";
+  ?>
+</body>
+</html>
