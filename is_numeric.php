7201dd6450fbe667d3b435f2e519be53e7b593d2
Gabriel Gomes Tatsch <gomestatschgabriel@gmail.com>
2022-12-01 12:08:36 -0300

    Table finished.

--- is_numeric.php
+++ is_numeric.php
@@ -10,19 +10,17 @@
 </head>
 <body>
   <?php
-    $num = 0;
-    $imp = $_POST["table"];
-    
-    if(isset($imp)) {
-      if(!is_numeric($imp)) {
-        echo "It isn't a number. Enter a number.";
-      } elseif(is_numeric($num)) {
-        for($mult = 0; $mult < 10; ++$mult ) {
-          $tab = $num * $mult;
-          echo `${$num} * ${$mult} = ${$tab}`;  
-        } 
-      }
+  if(isset($_POST["table"])) {
+    $num = $_POST["table"];
+    if(!is_numeric($num)) {
+      echo "It isn't a number. Enter a number to know his table.";
+    } else {
+      for($mult = 1; $mult <= 10; $mult++ ) {
+        $tab = $num * $mult;
+        echo "$num * $mult = $tab<br>";  
+      } 
     }
+  }
   ?>
   <form method="POST">
     <input type="text" name="table" placeholder="Enter a number">
